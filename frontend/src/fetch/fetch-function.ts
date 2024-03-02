46f416071a99879c88398138863c0c5037761b71
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-02 15:52:14 -0300

    :sparkles: feat: Add initial fetch function

---
+++ frontend/src/fetch/fetch-function.ts
@@ -0,0 +1,69 @@
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  GenericAbortSignal,
+} from "axios";
+
+type TypeType = "post" | "get" | "patch" | "put" | "delete";
+
+type fetchFunctionTypes<T> = {
+  url: string;
+  type?: TypeType;
+  body?: T;
+  options?: AxiosRequestConfig;
+  signal?: GenericAbortSignal | undefined;
+};
+
+/**
+ *
+ * @param url Url da api
+ * @param type tipo de requisição(get,patch,post,delete)
+ * @param body Corpo da requisição
+ * @param options Opções do axios
+ * @param signal Parametro para abortar a request
+ * @returns Retorna a resposta ou da um throw no erro
+ */
+
+const fetchFunction = async <T, R>({
+  url = "",
+  type = "get",
+  body = undefined,
+  options = undefined,
+  signal = undefined,
+}: fetchFunctionTypes<T>): Promise<R> => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  try {
+    if (type === "get" || type === "delete") {
+      const response: AxiosResponse<R> = await axios[type](
+        `${apiUrl}/api/v1${url}`,
+        {
+          signal,
+          headers: {
+            "content-Type": "application/json",
+          },
+          ...options,
+        },
+      );
+      return response.data;
+    }
+
+    const response: AxiosResponse<R> = await axios[type](
+      `${apiUrl}/api/v1${url}`,
+      body,
+      {
+        signal,
+        headers: {
+          "content-Type": "application/json",
+        },
+        ...options,
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default fetchFunction;
