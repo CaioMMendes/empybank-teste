4f9f85c9760df12649feec493eaf832094c19dc2
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-02 13:30:02 -0300

    :sparkles: feat: Add config-links component

---
+++ frontend/src/components/sidebar/config-links.tsx
@@ -0,0 +1,23 @@
+import { PiSun, PiUserCircle } from "react-icons/pi";
+import { Button } from "../ui/button";
+import avatarImage from "/avatarImage.png";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+
+const ConfigLinks = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <Button variant={"icon"} size={"icon"}>
+        <PiUserCircle size={24} />
+      </Button>
+      <Button variant={"icon"} size={"icon"}>
+        <PiSun size={24} />
+      </Button>
+      <Avatar className="size-9">
+        <AvatarImage src={avatarImage} />
+        <AvatarFallback>A</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
+
+export default ConfigLinks;
