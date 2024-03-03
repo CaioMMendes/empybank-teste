7aa6fc15843426923f26fb437f77ba07e5521076
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-03 14:31:09 -0300

    :sparkles: feat: Add get all assistant route

---
+++ backend/src/controller/assistant/get-all-assistant-controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import { getAllAssistantService } from "../../services/assistant/get-all-assistant-service";
+import AppError from "../../utils/appError";
+
+export const getAllAssistantController = catchAsync(
+  async (request: Request, response: Response, next: NextFunction) => {
+    const assistantResponse = await getAllAssistantService();
+
+    if (assistantResponse?.status !== "success") {
+      return next(
+        new AppError(
+          assistantResponse?.message ||
+            "Ocorreu um erro ao tentar buscar os assistentes",
+          400
+        )
+      );
+    }
+
+    return response.status(200).json({
+      status: "success",
+      statusCode: 200,
+      message: assistantResponse?.message,
+      data: { ...assistantResponse?.data },
+    });
+  }
+);
---
+++ backend/src/services/assistant/get-all-assistant-service.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../../lib/prisma";
+import AppError from "../../utils/appError";
+
+export const getAllAssistantService = async () => {
+  try {
+    const assistant = await prisma.assistant.findMany({
+      where: {},
+      orderBy: {
+        name: "asc",
+      },
+    });
+    return {
+      status: "success",
+      message: "Assistentes encontrados com sucesso",
+      data: { assistant },
+    };
+  } catch (error) {
+    console.log(error);
+    throw new AppError(
+      "Ocorreu um erro ao tentar procurar os assistentes",
+      400
+    );
+  }
+};
