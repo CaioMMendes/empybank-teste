b65a89412408d65b365dd3e494c568f1d3723295
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-03 14:31:35 -0300

    :sparkles: feat: Add get unique assistant route

---
+++ backend/src/controller/assistant/get-unique-assistant-controller.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import AppError from "../../utils/appError";
+import { getUniqueAssistantService } from "../../services/assistant/get-unique-assistant-service";
+
+export const getUniqueAssistantController = catchAsync(
+  async (request: Request, response: Response, next: NextFunction) => {
+    const { id } = request.params;
+
+    if (!id) {
+      return new AppError(
+        "Informe um id na url para que possa ser encontado o assistente",
+        400
+      );
+    }
+
+    const assistantResponse = await getUniqueAssistantService(id);
+
+    if (assistantResponse?.status !== "success") {
+      return next(
+        new AppError(
+          assistantResponse?.message ||
+            "Ocorreu um erro ao tentar encontrar o assistente",
+          400
+        )
+      );
+    }
+
+    return response.status(200).json({
+      status: "success",
+      statusCode: 200,
+      message: assistantResponse?.message,
+      data: { ...assistantResponse?.data },
+    });
+  }
+);
---
+++ backend/src/services/assistant/get-unique-assistant-service.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../../lib/prisma";
+import AppError from "../../utils/appError";
+
+export const getUniqueAssistantService = async (id: string) => {
+  try {
+    const assistant = await prisma.assistant.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        Client: {
+          orderBy: [{ name: "asc" }, { network: "asc" }, { code: "asc" }],
+        },
+      },
+    });
+    return {
+      status: "success",
+      message: "Assistente encontrado com sucesso",
+      data: { assistant },
+    };
+  } catch (error) {
+    console.log(error);
+    throw new AppError("Ocorreu um erro ao tentar encontrar o assistente", 400);
+  }
+};
