93c577a82bd3cb39333a60aeaaf6d5917d719276
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-04 01:33:54 -0300

    :sparkles: feat: Add fetch get unique assistants function

---
+++ frontend/src/fetch/assistant/get-unique-assistant.ts
@@ -0,0 +1,37 @@
+import { GenericAbortSignal } from "axios";
+import fetchFunction from "../fetch-function";
+import { z } from "zod";
+import { assistantFormSchema } from "@/components/assistant-register/types/assistant-form-schema";
+import { clientFormSchema } from "@/components/client-register/types/client-form-schema";
+
+type AssistantFormData = z.infer<typeof assistantFormSchema>;
+type ClientFormData = z.infer<typeof clientFormSchema>;
+
+type ClientResponse = {
+  id: string;
+  createdAt: Date;
+  AssistantId: string;
+} & ClientFormData;
+
+export type GetUniqueAssistantResponse = {
+  id: string;
+  createdAt: Date;
+  Client: ClientResponse[];
+} & AssistantFormData;
+
+export type GetUniqueDataResponse = {
+  data: {
+    assistant: GetUniqueAssistantResponse;
+  };
+};
+
+export const getUniqueAssistant = async (
+  assistantId: string,
+  signal: GenericAbortSignal,
+) => {
+  const response: GetUniqueDataResponse = await fetchFunction({
+    url: `/assistant/get/${assistantId}`,
+    signal,
+  });
+  return response;
+};
