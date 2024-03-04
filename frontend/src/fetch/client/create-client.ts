9bb4c1c5af0a861ac1bd42d1ddc98a126bb5c112
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-04 01:34:08 -0300

    :sparkles: feat: Add fetch create client function

---
+++ frontend/src/fetch/client/create-client.ts
@@ -0,0 +1,38 @@
+import { GenericAbortSignal } from "axios";
+import fetchFunction from "../fetch-function";
+import { clientFormSchema } from "@/components/client-register/types/client-form-schema";
+import { z } from "zod";
+
+type ClientFormData = z.infer<typeof clientFormSchema>;
+
+export type NewClientResponse = {
+  id: string;
+  createdAt: Date;
+} & ClientFormData;
+
+export type CreateClientDataResponse = {
+  data: {
+    client: NewClientResponse;
+  };
+};
+
+/**
+ *
+ * @param newClient Objeto que contem os dados do cliente a ser criado
+ * @param signal Signal para realizar o abort da requisição
+ * @returns retorna o cliente criado
+ */
+
+export const createClient = async (
+  newClient: ClientFormData,
+  signal: GenericAbortSignal,
+) => {
+  const response: CreateClientDataResponse = await fetchFunction({
+    url: "/client/create",
+    type: "post",
+    body: newClient,
+    signal,
+  });
+
+  return response;
+};
