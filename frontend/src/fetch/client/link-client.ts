baa74ba7a3733da76d632ea1a9d4193fce881217
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-04 15:35:22 -0300

    :sparkles: feat: Add type to link client and add signal

--- frontend/src/fetch/client/link-client.ts
+++ frontend/src/fetch/client/link-client.ts
@@ -1,19 +1,33 @@
+import { GenericAbortSignal } from "axios";
 import fetchFunction from "../fetch-function";
+
+export type LinkClientDataResponse = {
+  data: {
+    client: {
+      count: number;
+    };
+  };
+};
 
 /**
  *
  * @param assistantId id do assistente onde vão ser adicionados os clientes
  * @param clientsId array com os ids dos clientes que vão ser associados com o assistente
- * @returns
+ * @param signal signal para abortar a requisição em caso de multiplas requests
+ * @returns retorna o numero de clientes associados
  */
 
-export const linkClient = async (assistantId: string, clientsId: string[]) => {
-  const response = await fetchFunction({
+export const linkClient = async (
+  assistantId: string,
+  clientsId: string[],
+  signal: GenericAbortSignal,
+) => {
+  const response: LinkClientDataResponse = await fetchFunction({
     url: `/client/link/${assistantId}`,
     type: "patch",
-    body: clientsId,
+    body: [...clientsId],
+    signal,
   });
 
-  //todo o metodo updatemany não retorna nada, se precisar retornar vai ter que fazer outra consulta
   return response;
 };
