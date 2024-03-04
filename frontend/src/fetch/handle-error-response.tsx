dcfbeb83f9414655f326f4854fa358d0501d465b
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-04 15:46:58 -0300

    :sparkles: feat: Add handle error response function

---
+++ frontend/src/fetch/handle-error-response.tsx
@@ -0,0 +1,24 @@
+import { toastError } from "@/components/toast";
+
+export type ErrorWithResponse = {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+} & Error;
+
+export function handleErrorResponse(
+  error: ErrorWithResponse,
+  defaultMessage: string = "Ocorreu um erro",
+  callback?: () => void,
+) {
+  console.log(error);
+  if (error?.name === "CanceledError") {
+    return toastError("A requisição foi cancelada");
+  }
+
+  const errorMessage = error?.response?.data?.message;
+  toastError(errorMessage || defaultMessage);
+  callback && callback();
+}
