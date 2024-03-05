29883b6eff243acffbb865afea7b68246c8bb188
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-04 23:44:33 -0300

    :sparkles: feat: Add setQueryData to client-form component

--- frontend/src/components/client-register/client-form.tsx
+++ frontend/src/components/client-register/client-form.tsx
@@ -9,13 +9,14 @@ import {
 } from "../ui/alert-dialog";
 import { Input, InputContainer, InputLabel, InputLabelText } from "../ui/input";
 import { clientFormSchema } from "./types/client-form-schema";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   CreateClientDataResponse,
   NewClientResponse,
   createClient,
 } from "@/fetch/client/create-client";
 import { toastError, toastSuccess } from "../toast";
+import { ClientDataType } from "../assistant-clients-section/assistant-clients-section";
 
 type ClientFormProps = {
   setIsModalOpen: Dispatch<SetStateAction<boolean>>;
@@ -33,6 +34,7 @@ type ErrorWithResponse = {
 
 const ClientForm = ({ setIsModalOpen }: ClientFormProps) => {
   const abortControllerRef = useRef(new AbortController());
+  const queryClient = useQueryClient();
 
   const {
     register,
@@ -50,7 +52,7 @@ const ClientForm = ({ setIsModalOpen }: ClientFormProps) => {
   >({
     mutationFn: (newClient) =>
       createClient(newClient, abortControllerRef.current.signal),
-    onSuccess: (data) => handleSuccessResponse(data?.data?.client),
+    onSuccess: (data) => handleSuccessResponse(data?.data),
     onError: handleErrorResponse,
   });
 
@@ -64,8 +66,28 @@ const ClientForm = ({ setIsModalOpen }: ClientFormProps) => {
     mutate(data);
   };
 
-  function handleSuccessResponse(data: NewClientResponse) {
-    console.log(data);
+  function handleSuccessResponse(newClient: NewClientResponse) {
+    console.log(newClient);
+
+    queryClient.setQueryData(
+      ["getUnlinkedClients"],
+      (oldUnlinkedClients: ClientDataType) => {
+        const oldAssistantData = oldUnlinkedClients?.data?.client;
+
+        if (newClient && oldAssistantData !== undefined) {
+          const newData = [newClient, ...oldAssistantData];
+          //todo talvez fazer o sort aqui
+          //   newData.sort((a, b) => a.name.localeCompare(b.name));
+          console.log(newData);
+          return {
+            ...oldUnlinkedClients,
+            data: { ...oldUnlinkedClients.data, client: newData },
+          };
+        }
+        return oldUnlinkedClients;
+      },
+    );
+
     toastSuccess("Cliente cadastrado");
     reset();
   }
