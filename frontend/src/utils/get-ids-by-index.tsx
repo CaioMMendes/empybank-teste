0b0e0b4e0e0d078ce6a1503cffd519d98d1d48a1
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-05 15:38:15 -0300

    :sparkles: feat: Add getIdsByIndex function

---
+++ frontend/src/utils/get-ids-by-index.tsx
@@ -0,0 +1,37 @@
+import { clientFormSchema } from "@/components/unlinked-clients-section/client-register/types/client-form-schema";
+import { z } from "zod";
+
+type ClientFormData = z.infer<typeof clientFormSchema>;
+
+type ClientsResponse = {
+  assistantId: string | null;
+  id: string;
+  createdAt: Date;
+} & ClientFormData;
+
+/**
+ *
+ * @param index Índices a serem encontrados no array
+ * @param clients Array de clientes
+ * @returns Retorna um array contendo os ids clientes correspondentes aos índices
+ */
+
+export const getIdByIndex = (
+  index: string[],
+  clients: ClientsResponse[] | undefined,
+) => {
+  if (!clients) {
+    return [];
+  }
+
+  const ids = new Set<string>();
+
+  for (let i = 0; i < clients.length; i++) {
+    if (index.includes(String(i))) {
+      const client = clients[i];
+      ids.add(client.id);
+    }
+  }
+
+  return Array.from(ids);
+};
