b9be9b0e372f22020e1f2553b36936f76282834a
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-03-05 12:52:33 -0300

    :sparkles: feat: Add mutation success to assistent clients section

--- frontend/src/components/assistant-clients-section/assistant-clients-section.tsx
+++ frontend/src/components/assistant-clients-section/assistant-clients-section.tsx
@@ -6,16 +6,17 @@ import {
   ErrorWithResponse,
   handleErrorResponse,
 } from "@/fetch/handle-error-response";
-import { useDebounce } from "@/hooks/use-debounce";
 import useSelectedAssistantStore from "@/stores/selected-assistant";
+import { getClientByIndex } from "@/utils/get-client-by-index";
+import { getIdByIndex } from "@/utils/get-ids-by-index";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { PiArrowCircleLeft, PiMagnifyingGlass } from "react-icons/pi";
+import { useRef, useState } from "react";
+import { PiArrowCircleLeft } from "react-icons/pi";
+import { columns } from "../columns-table";
+import { DataTable } from "../data-table";
 import { toastSuccess } from "../toast";
 import { Button } from "../ui/button";
-import { InputIconContainer, InputWithIcon } from "../ui/input";
-import { DataTable } from "../data-table";
-import { columns } from "../columns-table";
+import { Loading } from "../loading";
 
 type ClientType = {
   id: string;
@@ -37,23 +38,11 @@ export type ClientDataType = {
 
 const AssistantClientsSection = () => {
   const abortControllerRef = useRef(new AbortController());
-  const [searchInput, setSearchInput] = useState("");
   const [rowSelection, setRowSelection] = useState({});
   const rowKeys = Object.keys(rowSelection);
   const queryClient = useQueryClient();
-  const debounceSearch = useDebounce(searchInput);
-  const { selectedAssistant } = useSelectedAssistantStore();
-  useEffect(() => {
-    // if (isSuccess) {
-    // console.log(unlinkedClientsData);
-    // console.log(searchClient(debounceSearch, unlinkedClientsData.data.client));
-    // }
-    //eslint-disable-next-line
-  }, [debounceSearch]);
-
-  const handleSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(e.target.value);
-  };
+  const { selectedAssistant, unlinkSelectedAssistantClients } =
+    useSelectedAssistantStore();
 
   const { mutate, isPending } = useMutation<
     UnlinkClientDataResponse,
@@ -72,23 +61,16 @@ const AssistantClientsSection = () => {
 
   function handleSuccessResponse(count: number | null | undefined) {
     if (count) {
-      const newClients = [
-        {
-          id: `${Date.now()}`,
-          name: "dasdas",
-          code: "dasdas",
-          network: "dasdas",
-          createdAt: "dasdas",
-        },
-      ];
-
+      const clients = getClientByIndex(rowKeys, selectedAssistant?.Client);
+      const ids = getIdByIndex(rowKeys, selectedAssistant?.Client);
+      unlinkSelectedAssistantClients(ids);
       queryClient.setQueryData(
         ["getUnlinkedClients"],
         (oldUnlinkedClients: ClientDataType) => {
           const oldAssistantData = oldUnlinkedClients?.data?.client;
 
-          if (newClients && oldAssistantData !== undefined) {
-            const newData = [...newClients, ...oldAssistantData];
+          if (clients && oldAssistantData !== undefined) {
+            const newData = [...clients, ...oldAssistantData];
             //   newData.sort((a, b) => a.name.localeCompare(b.name));
             console.log(newData);
             return {
@@ -99,16 +81,14 @@ const AssistantClientsSection = () => {
           return oldUnlinkedClients;
         },
       );
-
+      setRowSelection({});
       return toastSuccess(`${count} clientes desvinculados com sucesso`);
     }
   }
 
   const handleUnlinkClients = () => {
-    mutate([
-      "618a65cd-aecd-4552-a60f-e795fc67fa8a",
-      "36fb09cf-efb6-4598-99e8-4652a7f5d6ca",
-    ]);
+    const clientIds = getIdByIndex(rowKeys, selectedAssistant?.Client);
+    mutate(clientIds);
   };
   console.log(selectedAssistant);
 
@@ -127,36 +107,29 @@ const AssistantClientsSection = () => {
               "Selecione um assistente"
             )}
           </h2>
-          <span className="flex flex-nowrap items-center justify-center rounded-full px-3 text-sm text-primary ring-1 ring-interactive-secondary">
-            {rowKeys?.length || 0}
-            {" / "}
-            {selectedAssistant?.Client.length || 0}
-          </span>
+          {selectedAssistant?.Client && (
+            <span className="flex flex-nowrap items-center justify-center rounded-full px-3 text-sm text-primary ring-1 ring-interactive-secondary">
+              {rowKeys?.length || 0}
+              {" / "}
+              {selectedAssistant?.Client.length || 0}
+            </span>
+          )}
         </div>
-        <div className="flex w-fit items-center gap-2">
-          <Button
-            variant={"destructive"}
-            className="rounded-lg leading-[150%] "
-            onClick={handleUnlinkClients}
-            disabled={isPending}
-          >
-            Desvincular
-            <PiArrowCircleLeft size={15} />{" "}
-          </Button>
-        </div>
+
+        {selectedAssistant?.Client && (
+          <div className="flex w-fit items-center gap-2">
+            <Button
+              variant={"destructive"}
+              className="rounded-lg leading-[150%] "
+              onClick={handleUnlinkClients}
+              disabled={isPending}
+            >
+              Desvincular
+              <PiArrowCircleLeft size={15} />{" "}
+            </Button>
+          </div>
+        )}
       </div>
-      {/* search
-      <InputIconContainer Icon={PiMagnifyingGlass}>
-        <InputWithIcon
-          placeholder="Buscar"
-          value={searchInput}
-          onChange={handleSearchInputChange}
-        />
-      </InputIconContainer>
-      {selectedAssistant &&
-        selectedAssistant?.Client.map((client) => {
-          return <p key={client.id}>{client.name}</p>;
-        })} */}
 
       {selectedAssistant?.Client && (
         <DataTable
